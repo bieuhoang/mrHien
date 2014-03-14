dc5713a44074d566c5038fb26c53067920cacc55
Hoang bieu <bieu.hv@gmail.com>
2014-03-14 16:21:53 +0700

    DA

--- public_html/protected/views/site/sanpham.php
+++ public_html/protected/views/site/sanpham.php
@@ -14,18 +14,10 @@
  » <a href="<?php echo Yii::app()->request->baseUrl."/site/sp?i=".$pas->id;?>"><?php echo $pas->name;?></a></h2></div>
 <div style="clear:both; font-size:11px; border-bottom:1px solid #ccc; padding:6px 0 6px 9px; margin-bottom:12px;"> 
 </div>
-		<table border="0" cellpadding="0" cellspacing="0" height="163" width="456">
-			<tbody>
-				<tr>
-					<td style="width: 150px;">
-						<img alt="<?php echo $pas->name?>" src="<?php echo Yii::app()->request->baseUrl."/files/images/".$pas->image;?>" style="width: 135px; height: 159px; margin-left: 0px; margin-right: 0px;">
-					</td>
-					<td>
-					<span style="color: rgb(255, 0, 0);"><strong><?php echo $pas->name;?></strong></span></br>
-					<span><?php echo $pas->noidung?></td>
-				</tr>
-			</tbody>
-		</table>
+<span style="color: rgb(255, 0, 0);"><strong><?php echo $pas->name;?></strong></span></br>
+<img alt="<?php echo $pas->name?>" src="<?php echo Yii::app()->request->baseUrl."/files/images/".$pas->image;?>" style="float: left; width: 135px; height: 159px; margin: 10px;">
+<div style="font-size: 14;font-weight: bold;color: #005fa3; margin: 10px;"><?php echo $pas->tomtat;?></div>
+<span><?php echo $pas->noidung?></td>
 <?php
 	}
 ?>
